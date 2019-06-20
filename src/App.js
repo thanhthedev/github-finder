00023e36797cd5092e802168112728417b2de059
thanhthedev <huythanhdao@gmail.com>
2019-06-20 15:52:11 +1000

    added search functionality

--- src/App.js
+++ src/App.js
@@ -11,19 +11,25 @@ class App extends React.Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   console.log(res.data);
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  handlesearchUsers = async text => {
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     console.log(res.data);
-    this.setState({ users: res.data, loading: false });
-  }
-
-  handlesearchUsers = text => {
-    console.log(text);
+    this.setState({ users: res.data.items, loading: false });
   };
   render() {
     return (
