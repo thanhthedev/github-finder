a907d3975a943867750bfffb43b5b7d8d570a7fa
thanhthedev <huythanhdao@gmail.com>
2019-06-20 16:25:25 +1000

    added a clear button

--- src/App.js
+++ src/App.js
@@ -11,18 +11,10 @@ class App extends React.Component {
     loading: false
   };
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${
-  //       process.env.REACT_APP_GITHUB_CLIENT_ID
-  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   console.log(res.data);
-  //   this.setState({ users: res.data, loading: false });
-  // }
+  // Search users
 
   handlesearchUsers = async text => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
@@ -31,12 +23,19 @@ class App extends React.Component {
     console.log(res.data);
     this.setState({ users: res.data.items, loading: false });
   };
+
+  // Clear Users
+  handleclearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search searchUsers={this.handlesearchUsers} />
+          <Search
+            searchUsers={this.handlesearchUsers}
+            clearUsers={this.handleclearUsers}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
